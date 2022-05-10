8cef36d749eb39d57e36c11980e304df73a87f68
Maxim <Maxim.polkin@gmail.com>
2022-05-10 17:35:51 +0400

    Animation script
    Allows you to render sprites at a
    specific frame rate per second.

---
+++ SpriteAnimation.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+    [RequireComponent(typeof(SpriteRenderer))]   // для автоматического довабления SpriteRenderer
+
+    public class SpriteAnimation : MonoBehaviour
+    {
+
+        [SerializeField] private int _frameRate;
+        [SerializeField] private bool _loop;
+        [SerializeField] private Sprite[] _sprites;
+        [SerializeField] private UnityEvent _onComplete;    // ивент по прошествии анимации
+
+        private SpriteRenderer _renderer;
+        private float _secondsPerFrame;
+        private int _curentSpriteIndex;         // порядок текущего спрайта
+        private float _nextFrameTime;           // время, когда нужно обновить спрайт
+        private bool _isPlaying = true;         // флаг остановки проигрывания анимации
+
+        private void Start()
+        {
+            _renderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void OnEnable()
+        {
+            _secondsPerFrame = 1f / _frameRate;           //инициализация переменной 
+            _nextFrameTime = Time.time + _secondsPerFrame;
+            _curentSpriteIndex = 0;
+        }
+
+        private void Update()
+        {
+            if (!_isPlaying || _nextFrameTime > Time.time) return;
+
+            if (_curentSpriteIndex >= _sprites.Length)
+            {
+                if (_loop)
+                {
+                    _curentSpriteIndex = 0;
+                }
+                else
+                {
+                    _isPlaying = false;
+                    _onComplete?.Invoke();
+                    return;                 // выход из цикла если не зажат флаг loop
+                }
+            }
+
+            _renderer.sprite = _sprites[_curentSpriteIndex];
+            _nextFrameTime += _secondsPerFrame;
+            _curentSpriteIndex++;
+
+        }
+    }
